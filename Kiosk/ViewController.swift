eb05afb0e13667412c893c6e0cf20d5a666dbd76
chhue <chhue96@gmail.com>
2024-11-25 11:21:35 +0900

    chore: 프로젝트 추가

---
+++ Kiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiosk
+//
+//  Created by CHYUN on 11/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
