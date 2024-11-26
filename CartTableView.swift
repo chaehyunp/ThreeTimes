b1858487a4e112228587cad0cbd33bdcdc199a13
jbnong07 <jbnong07@naver.com>
2024-11-26 13:56:26 +0900

    init: 테이블뷰, 테이블뷰셀 파일 생성

---
+++ CartTableView.swift
@@ -0,0 +1,20 @@
+//
+//  CartTableView.swift
+//  Kiosk
+//
+//  Created by 박진홍 on 11/26/24.
+//
+
+import UIKit
+
+class CartTableView: UITableView {
+    
+    init() {
+        super.init(frame: .zero, style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
